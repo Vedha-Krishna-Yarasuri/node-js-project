f6d4b52cb0b2b5e55ed1d9b112e96f743cb6c2c0
vedha krishna <49089434+Vedha-Krishna-Yarasuri@users.noreply.github.com>
2019-08-29 11:09:11 +0530

    Create saving-the-input.js
    Add the input from chatbot to a new file

---
+++ saving-the-input.js
@@ -0,0 +1,29 @@
+var fs=require("fs");
+var telegram=require("telegram-bot-api");
+var api = new telegram({
+	token:'< YOUR _ TELEGRAM _ API>',
+	updates: {
+        	enabled: true
+    }
+});
+
+api.on("message",function(message)
+{
+
+	
+		var str=fs.readFileSync("hello.txt").toString();
+		var s=message.chat.id.toString()+":"+message.text.toLowerCase();
+		var search=str.split(",")
+		var pos=search.includes(s);
+
+		console.log(message.text)
+		if(pos==true){
+			console.log(pos);
+			api.sendMessage({chat_id:message.chat.id,text:"Name aldready exist"});
+		}
+		else
+		{
+			console.log("i am here");
+			fs.appendFileSync("hello.txt",s+",");
+		}
+});
